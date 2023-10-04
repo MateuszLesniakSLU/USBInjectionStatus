df7b07228530cc011375b354fa12db775cd254ae
mateuszlesniakslu <vuuuda@gmail.com>
2023-10-04 19:40:28 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Management;
+
+namespace USBDeviceWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Monitorowanie podłączenia urządzenia USB...");
+
+            ManagementEventWatcher watcher = new ManagementEventWatcher();
+            watcher.Query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2");
+
+            watcher.EventArrived += Watcher_EventArrived;
+
+            watcher.Start();
+
+            Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć.");
+            Console.ReadKey();
+
+            watcher.Stop();
+
+            Console.ReadKey();
+        }
+
+        private static void Watcher_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            Console.WriteLine("Urządzenie USB zostało podłączone!");
+        }
+    }
+}
